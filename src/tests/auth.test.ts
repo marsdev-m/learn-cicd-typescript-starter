7f97e2ea1429000eabaa50b4249ec9efb12fc69d
Marius <marius.meyer.it@gmail.com>
2026-03-06 13:36:09 +0100

    added prettier

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -1,18 +1,17 @@
 import { describe, expect, test } from "vitest";
 import { getAPIKey } from "../api/auth";
 
-
 // mock request
 const reqMock = {
-    headers: {
-        'authorization': 'ApiKey 123-456-789'
-    }
-}
+  headers: {
+    authorization: "ApiKey 123-456-789",
+  },
+};
 
-describe('APIKey', () => {
-    test('returns API key from header', () => {
-        const APIKey = getAPIKey(reqMock['headers']);
-        expect(APIKey).toBeTypeOf('string');
-        expect(APIKey).toBe('123-456-789');
-    });
+describe("APIKey", () => {
+  test("returns API key from header", () => {
+    const APIKey = getAPIKey(reqMock["headers"]);
+    expect(APIKey).toBeTypeOf("string");
+    expect(APIKey).toBe("123-456-789");
+  });
 });
